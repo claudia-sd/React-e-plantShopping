9c4308ca217d3fbfcd9b6c1e93a457d53c532f2e
Clau <claudiasentinelli@gmail.com>
2024-09-23 13:14:54 -0400

    Front Page

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -276,8 +276,8 @@ const handlePlantsClick = (e) => {
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 style={{color:'white'}}>Evergreen Nursery</h3>
+                    <i style={{color:'white'}}>Where Your Gardening Dreams Take Root</i>
                     </div>
                     </a>
                 </div>
