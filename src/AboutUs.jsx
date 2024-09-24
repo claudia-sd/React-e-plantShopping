ef2310ba2e899d6f86203790fcd92ecc58aa3b0d
claudia-sd <claudiasentinelli@gmail.com>
2024-09-23 20:14:56 -0400

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './AboutUs.css';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CreateSlice';
 
 function AboutUs() {
+  
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
       <p className="about-us-description">Welcome to Evergreen Nursery, where your gardening dreams take root!</p>
       <p className="about-us-content">
-        At Evergreen Nursery, we believe that every plant has a story to tell. Our wide selection features a diverse array of trees, shrubs, perennials, and annuals, all carefully cultivated to thrive in our local climate.
+      At Evergreen Nursery, we believe that every plant has a story to tell. Our wide selection features a diverse array of trees, shrubs, perennials, and annuals, all carefully cultivated to thrive in our local climate.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-      Whether you’re looking to create a vibrant garden, enhance your landscape, or simply add a touch of green to your home, our knowledgeable staff is here to guide you in choosing the perfect plants to suit your unique needs.
+        Whether you’re looking to create a vibrant garden, enhance your landscape, or simply add a touch of green to your home, our knowledgeable staff is here to guide you in choosing the perfect plants to suit your unique needs.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-      Visit us at Evergreen Nursery, where we’re not just growing plants; we’re cultivating relationships with our customers and the community. Join us in celebrating the joys of gardening, and let’s grow together!
+        Visit us at Evergreen Nursery, where we’re not just growing plants; we’re cultivating relationships with our customers and the community. Join us in celebrating the joys of gardening, and let’s grow together!
       </p>
     </div>
   );
